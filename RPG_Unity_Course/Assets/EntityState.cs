c5f22474afb9d4ad841e2615de94efbfd6f46b61
QuentinZoli <quentin_liso@live.fr>
2025-07-31 12:03:25 +0200

    Animation setup

--- RPG_Unity_Course/Assets/EntityState.cs
+++ RPG_Unity_Course/Assets/EntityState.cs
@@ -4,30 +4,33 @@ public abstract class EntityState
 {
 	protected Player player;
 	protected StateMachine stateMachine;
-	protected string stateName;
+	protected string animBoolName;
+	protected Animator anim;
 
-	public EntityState(Player player, StateMachine stateMachine, string stateName)
+	public EntityState(Player player, StateMachine stateMachine, string animBoolName)
 	{
 		this.player = player;
 		this.stateMachine = stateMachine;
-		this.stateName = stateName;
+		this.animBoolName = animBoolName;
+
+		anim = player.anim;
 	}
 
 	// Called everytime the state is changed
 	public virtual void Enter()
 	{
-		Debug.Log("I enter " + stateName);
+		anim.SetBool(animBoolName, true);
 	}
 
 	// Run the logic of the state here
 	public virtual void Update()
 	{
-		Debug.Log("I run update of " + stateName);
+		Debug.Log("I run update of " + animBoolName);
 	}
 
 	// Called everytime we exit a state and change to a new one
 	public virtual void Exit()
 	{
-		Debug.Log("I exit " + stateName);
+		anim.SetBool(animBoolName, false);
 	}
 }
--- RPG_Unity_Course/Assets/Player.cs
+++ RPG_Unity_Course/Assets/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public Animator anim { get; private set; }
 	private PlayerInputSet input;
 	private StateMachine stateMachine;
 	public Player_IdleState idleState { get; private set; }
@@ -10,6 +11,7 @@ public class Player : MonoBehaviour
 
 	private void Awake()
 	{
+		anim = GetComponentInChildren<Animator>();
 		stateMachine = new StateMachine();
 		input = new PlayerInputSet();
 		idleState = new Player_IdleState(this, stateMachine, "idle");
@@ -36,6 +38,6 @@ public class Player : MonoBehaviour
 
 	private void Update()
 	{
-		stateMachine.currentState.Update();	
+		stateMachine.UpdateActiveState();	
 	}
 }
